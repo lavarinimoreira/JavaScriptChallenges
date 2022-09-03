1de2ff2def208cfb5a5e9e7b0e1ccb7ba689edf8
Gabriel Lavarini <lavarinimoreira@gmail.com>
2022-09-03 00:17:51 -0300

    solving challenge_3

---
+++ challenge_3.js
@@ -0,0 +1,18 @@
+// A pangram is a sentence that contains every letter of the alphabet, like:
+//"The quick brown fox jumps over the lazy dog"
+
+// Write a function called isPangram, which checks to see if a given sentence contains every letter of the alphabet.
+// Make sure you igore string casing!
+
+// isPangram('The five boxing wizards jump quickly') //true
+// isPangram('The five boxing wizards jump quick') //false
+
+function isPangram(arr){
+    const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    for(let characteresOfAlplhabet of alphabet){
+        const containsCharactere = arr.toUpperCase().indexOf(characteresOfAlplhabet);
+        if(containsCharactere === -1)
+            return false;
+    }
+    return true;
+}
